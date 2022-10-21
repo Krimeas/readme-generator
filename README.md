2d33a3fcc6bca9c025f7063bbb07f81620f301e3
Krimeas <krimeass@gmail.com>
2022-10-20 21:22:21 -0500

    added video and pictures, removed extra js file, generated Readme and edited through generator.  Added missed JS section.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# Readme Generator
+
+## Table of Contents
+  1. [Description](#description)
+  2. [Assigned Task](#assigned-tasks)
+  3. [Installation Instructions](#installation-instructions)
+  4. [Usage Instructions](#usage-instructions)
+  5. [Technologies Used](#technologies-used)
+  6. [Contributor Repository Links](#contributor-repository-links)
+  7. [Additional Sources](#additional-sources)
+
+-------------------------------------
 
 
 ## The MIT License
@@ -16,20 +28,60 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-## Description
-asdfasdfas
-## Table of Contents
-asfdasdfas
-## Installation Instructions
-asdfasdf
-## How to Use
-asdfasfdasfasdfasdf
-## Contributors githubs
-asdfasfas
-## Additional Sources
-asdfasfasdfasdfasdfasdf
 
-https://github.com/Krimeas
-Additional Sources:
-https://naereen.github.io/badges/
+-------------------------------------
+
+## Description
+A quick and easy way to generate a high quality readme by answering a series of questions.  By opening the terminal in throught the folder associated with this project.  From there, by typing 'node index.js' and hitting enter/return the questions will appear one after the other.  Once you have made it through the questions a high quality readme file will be written to the subfolder where the JS file is saved. 
+
+
+
+## Assigned Tasks
+  1. Write a Javascript file that uses inquirer to prompt questions to the user.
+  2. write up a series of prompts that will apply the given answers to data that will then be written to a Readme file.
+  3. Create and apply a video showing instructions on how to use the application.
+
+
+-------------------------------------
+
+### Installation Instructions
+Installation of the readme Generator can be completed through a github download of the files related to it. 
+
+
+![Portfolio](./assets/pic1.JPG)
+
+-------------------------------------
+
+### Usage Instructions
+![Portfolio](./assets/usage_instruction.webm)
+
+
+![Portfolio](./assets/pic1.JPG)
+![Portfolio](./assets/pic2.jpg)
+
+
+-------------------------------------
+
+#### Technologies Used
+1. JavaScript
+2. VsCode
+3. Node
+4. Inquirer
+
+
+#### Contributors githubs
+1. https://github.com/Krimeas
+
+
+#### Additional Sources
+1. Source of the Badges for the License: https://naereen.github.io/badges/
+2. Markdown link help: https://gist.github.com/jonschlinkert/ac5d8122bfaaa394f896#sub-heading
+
+
+
+Task details and initial code supplied by UT Austin Coding Bootcamp.
+
+
+
+
       
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+// The above usitilize the Inquirer and fs 'file system' modules that have either been downloaded or included wiht this version of node.
 
 // const generateMarkdown = require('./utils/generateMarkdown')
 
@@ -21,6 +22,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 `;
+
 // theUnlicense licesne badge, link, description
 const theUnlicense = `
 ## The Unlicense License
@@ -53,6 +55,7 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 
 `;
+
 // Creative Commons licesne badge, link, description
 const creativeCommons = `
 ## The Creative Commons License
@@ -68,106 +71,211 @@ The licensor cannot revoke these freedoms as long as you follow the license term
 `;
 
 
-
+// Generate Readme function that runs inquirer in your console once you open it and type 'node index.js'
+// error when selecting license, alwasy selects the 'one above' on the list in the terminal.
 function generateReadme() {
   inquirer
     .prompt([
       {
         type: 'input',
-        message: 'What is the title of your project?',
+        message: 'This series of questions will generate your Readme, please go slow and read each question carefully.  Press Enter to proceed now.',
+        name: 'REMINDER',
+      },
+      {
+        type: 'input',
+        message: 'What is the title of your application?',
         name: 'title',
       },
       {
         type: 'list',
-        message: 'what license will you be using?',
+        message: 'Which of these three licenses will you be using?',
         name: 'license',
         choices: [mitLicense, theUnlicense, creativeCommons],
       },
       {
         type: 'input',
-        message: 'Give a description of your project.',
+        message: 'Write a few sentences describing your application.',
         name: 'description',
       },
       {
         type: 'input',
-        message: 'List your Table of Contents',
-        name: 'table',
-      },
-      {
-        type: 'input',
-        message: 'Write Installation instructions here.',
+        message: 'Write a few sentences describing how to install your application.',
         name: 'installation',
       },
       {
         type: 'input',
-        message: 'List the usage of your project for the READme.',
+        message: 'Write how to use your application, or link a video from assets.  Example: ![Portfolio](/assets/codingQuizPic.JPG)',
         name: 'usage',
       },
       {
         type: 'input',
-        message: 'P1. If you have uploaded a picture into assets, put it here. Example: (/assets/codingQuizPic.JPG)',
+        message: 'The next three questions are requesting Pictures, press enter to pass through them if you have none.  Press Enter to proceed now.',
+        name: 'pics',
+      },
+      {
+        type: 'input',
+        message: 'P1. If you have uploaded a picture into assets, put it here. Example: ![Portfolio](/assets/picture1.JPG)',
         name: 'pic1',
       },
       {
         type: 'input',
-        message: 'P2. If you have uploaded a picture into assets, put it here. Example: (/assets/codingQuizPic.JPG)',
+        message: 'P2. If you have uploaded a picture into assets, put it here. Example: ![Portfolio](/assets/picture2.JPG)',
         name: 'pic2',
       },
       {
         type: 'input',
-        message: 'P3. If you have uploaded a picture into assets, put it here. Example: (/assets/codingQuizPic.JPG)',
+        message: 'P3. If you have uploaded a picture into assets, put it here. Example: ![Portfolio](/assets/picture3.JPG)',
         name: 'pic3',
       },
       {
         type: 'input',
-        message: 'the Next few questiosn will request contributors',
+        message: 'Over the next Four questions you will be asked for a list of used technologies for your application.  Group then when necessary.  Press enter to leave empty. Press Enter to proceed now.',
+        name: 'technologies',
+      },
+      {
+        type: 'input',
+        message: 'Did you use HTML, CSS, JavaScript? Example: 1. HTML, CSS',
+        name: 'tech1',
+      },
+      {
+        type: 'input',
+        message: 'Did you use any frameworks such as Bootstrap or a code editor such as VSCode? Example: 2. Foundation, Lime.',
+        name: 'tech2',
+      },
+      {
+        type: 'input',
+        message: 'Any special technologies can go here. Example: 3. node, Mocha',
+        name: 'tech3',
+      },
+      {
+        type: 'input',
+        message: 'Any special technologies can go here. Example: 4. ESLint, Husky',
+        name: 'tech4',
+      },
+      {
+        type: 'input',
+        message: 'The next four questions will ask for the github accounts of your contributors.  If you do not have any besides yourself, only put yourself in the first one.  Press enter to leave empty.  Press Enter to proceed now.',
         name: 'contributors',
       },
       {
         type: 'input',
-        message: 'List additional souurces and thier use in the project.',
+        message: 'First Contributors Github Repository.',
+        name: 'contributor1',
+      },
+      {
+        type: 'input',
+        message: 'Second Contributors Github Repository.',
+        name: 'contributor2',
+      },
+      {
+        type: 'input',
+        message: 'Third Contributors Github Repository.',
+        name: 'contributor3',
+      },
+      {
+        type: 'input',
+        message: 'Fourth Contributors Github Repository.',
+        name: 'contributor4',
+      },
+      {
+        type: 'input',
+        message: `Enter your email address so that people may contact you with questions.`,
+        name: 'questions',
+      },
+      {
+        type: 'input',
+        message: `List additional sources and their use in the application over the next five questions. Example: "1. General searching - www.google.com". Press Enter to proceed now.`,
         name: 'sources',
+      },
+      {
+        type: 'input',
+        message: 'List your first additional source the use in the application.',
+        name: 'source1',
+      },
+      {
+        type: 'input',
+        message: 'List your second additional source the use in the application.',
+        name: 'source2',
+      },
+      {
+        type: 'input',
+        message: 'List your third additional source the use in the application.',
+        name: 'source3',
+      },
+      {
+        type: 'input',
+        message: 'List your fourth additional source the use in the application.',
+        name: 'source4',
+      },
+      {
+        type: 'input',
+        message: 'List your fifth additional source the use in the application.',
+        name: 'source5',
       },
     ])
     .then((data) => {
+//  The variable the remaining data is buried in such as Table of contents, and the ## section titles.
+      let generated = `# ${data.title}
 
-      let generated = `
+## Table of Contents
+  1. Description
+  2. Installation Instructions
+  3. Usage Instructions
+  4. Technologies Used
+  5. Contributor Repository Links
+  6. Additional Sources
 
-#${data.title}
+-------------------------------------
 
 ${data.license}
+
+-------------------------------------
 
 ## Description
 ${data.description}
 
--------------------------------------
-
-## Table of Contents
-${data.table}
 
 ## Installation Instructions
 ${data.installation}
 
-## How to Use
+
+## Usage Instructions
 ${data.usage}
 
-![Portfolio]${data.pic1}
-![Portfolio]${data.pic2}
-![Portfolio]${data.pic3}
+
+${data.pic1}
+${data.pic2}
+${data.pic3}
 
 --------------------------------
 
-## Contributors githubs
-${data.contributors}
+### Technologies Used
+${data.tech1}
+${data.tech2}
+${data.tech3}
+${data.tech4}
 
-## Additional Sources
-${data.sources}
 
-https://github.com/Krimeas
-Additional Sources:
-https://naereen.github.io/badges/
+### Contributors githubs
+${data.contributor1}
+${data.contributor2}
+${data.contributor3}
+${data.contributor4}
+
+### Questions:
+${data.questions}
+
+### Additional Sources
+${data.source1}
+${data.source2}
+${data.source3}
+${data.source4}
+${data.source5}
+
+
+
       `;
-
+//    Writes the file with the data from the inquirer and 'generated' variable.
       fs.writeFile('readme.md', generated, (err) => {
         (err) ? console.log(err) : console.log("sucess!")
       })
